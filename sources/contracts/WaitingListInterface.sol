2555af376b9e4f7a7e30489f80cd4bab1d11c05c
Julian Malliaris <julian.e.malliaris@campus.tu-berlin.de>
2018-06-28 13:02:14 +0200

    minor changes

--- sources/contracts/WaitingListInterface.sol
+++ sources/contracts/WaitingListInterface.sol
@@ -5,4 +5,6 @@ interface isWaitingList {
 	function addRecipient (address adr, string bt, uint hla, bool accMM, uint signup, bool hp, uint age, uint region, uint country) public returns (address);
 
 	function removeRecipient (address a) public returns (address);
+
+	function addOrgan(address addr, string bt, uint age, uint region, uint country) public returns (address[20]);
 }
--- sources/migrations/2_deploy_contracts.js
+++ sources/migrations/2_deploy_contracts.js
@@ -1,11 +1,18 @@
 var Colloquium = artifacts.require("./Colloquium.sol");
 var ColloquiumUser = artifacts.require("./ColloquiumUser.sol");
 var WaitingList = artifacts.require("./WaitingList.sol");
-
+//var Transplantcenter= artifacts.require("./Transplantcenter.sol");
+//var Coordinatecenter= artifacts.require("./Coordinatercenter.sol");
+//for testing
+var ExposedWaitingList = artifacts.require("./ExposedWaitingList.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(Colloquium);
   deployer.link(Colloquium, [ColloquiumUser]);
   deployer.deploy(ColloquiumUser);
   deployer.deploy(WaitingList);
+ // deployer.deploy(Transplantcenter);
+ // deployer.deploy(Coordinatecenter);
+  //for testing
+  deployer.deploy(ExposedWaitingList);
 };

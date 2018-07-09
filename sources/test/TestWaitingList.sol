d57b748a3d3d38fa01fd8ee86d88c2231ea9b61a
Julian Malliaris <julian.e.malliaris@campus.tu-berlin.de>
2018-07-09 18:57:54 +0200

    tests

--- sources/test/TestWaitingList.sol
+++ sources/test/TestWaitingList.sol
@@ -33,10 +33,51 @@ contract TestWaitingList {
         Assert.equal(expectedAge, receivedAge, "Not equal");
     }
 
+    function testGetBloodtype() public {
+        string memory expectedBloodtype = "A";
+        string memory receivedBloodtype = waitinglist.getRecipientBloodtype(this);
+        Assert.equal(expectedBloodtype, receivedBloodtype, "Not equal");
+    }
+
     function testRemoveRecipient() public {
         address expected = this;
         address returnedAddress = waitinglist.removeRecipient(this);
         Assert.equal(returnedAddress, expected, "Address doesnt match");
+    }
+
+/*    function testAddWithUpdate() public {
+        address expected = 0x07F59D32DE9C7d4de034080536AA0E00Dfb3Df97;
+        address returned = waitinglist.updateRecipient(0x07F59D32DE9C7d4de034080536AA0E00Dfb3Df97, "B", 1, true, 1234, true, 20, 1, 10);
+        Assert.equal(expected, returned, "Not equal");
+
+        //address falseex = waitinglist.recipientsList[0].adr;
+        //assert(falseex != 0x07F59D32DE9C7d4de034080536AA0E00Dfb3Df97);
+    }
+*/
+
+    function testAddSeveralRecipients() public {
+        address expected = this;
+        address returnedAddress = waitinglist.addRecipient(this, "A", 1, true, 1234, true, 17, 1, 10);
+        Assert.equal(returnedAddress, expected, "Address doesnt match");
+
+        address expected1 = 0x07F59D32DE9C7d4de034080536AA0E00Dfb3Df97;
+        address returnedAddress1 = waitinglist.addRecipient(0x07F59D32DE9C7d4de034080536AA0E00Dfb3Df97, "B", 1, true, 1234, true, 17, 1, 10); 
+        Assert.equal(returnedAddress1, expected1, "Address doesnt match");
+
+        address expected2 = this;
+        address returnedAddress2 = waitinglist.removeRecipient(this);
+        Assert.equal(returnedAddress2, expected2, "Address doesnt match");
+
+    }
+
+    function testUpdateRecipient() public {
+        address expected = 0x07F59D32DE9C7d4de034080536AA0E00Dfb3Df97;
+        address returned = waitinglist.updateRecipient(0x07F59D32DE9C7d4de034080536AA0E00Dfb3Df97, "B", 1, true, 1234, true, 20, 1, 10);
+        Assert.equal(expected, returned, "Not equal");
+
+        uint exage = 20;
+        uint recage = waitinglist.getRecipientAge(0x07F59D32DE9C7d4de034080536AA0E00Dfb3Df97);
+        Assert.equal(exage, recage, "not equal");
     }
 
 //------------------POSSIBLE?-----------------
@@ -47,13 +88,15 @@ contract TestWaitingList {
     }
 
 
-/*-----------------HOW TO CHECK ADDRESSES IN LISTS?--------------
+//-----------------HOW TO CHECK ADDRESSES IN LISTS?--------------
     function testAddOrgan() public {
-        address expected = this;
-        address[20] returned = waitinglist.addOrgan(this, "A", 17, 1, 1);
-        Assert.equal(expected, returned[0].addr, "List doesnt match");
+        address[20] memory expected;
+        expected[0] = this;
+        address[20] memory returned = waitinglist.addOrgan(this, "A", 17, 1, 1);
+        Assert.equal(expected[0], returned[0], "List doesnt match");
+        waitinglist.removeRecipient(this);
     }
-*/
+
 
 /*
     function testCheckBloodtype() public {
